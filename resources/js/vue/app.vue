cd636067bb1ade97da229756f53dc62029b2e329
jiangwenjie1011 <8916242+jiangwenjie1011@user.noreply.gitee.com>
2021-07-27 17:41:40 +0800

    add:vue

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="todolistContainer">
+      <div class="heading">
+        <h2 id="title">Todolist</h2>
+        <add-item-form></add-item-form>
+      </div>
+      <list-view></list-view>
+      </div>  
+</template>
+
+<script>
+import AddItemForm from './addItemForm.vue'
+import ListView from './listView.vue'
+export default {
+   components: {AddItemForm, ListView}
+}
+</script>
+
+<style>
+.todolistContainer {
+  width: 300px;
+  margin: auto;
+}
+</style>
